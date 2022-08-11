8dc297c498df4a461f49868cf02b9e89c31061bc
yushaushaibu <yushaushaibu1@gmail.com>
2022-08-11 16:36:45 -0700

    Video details and subtitle added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,36 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from "../apis/youtube";
 
+
+
+
 class App extends React.Component {
-    onTermSearch = (term) => {
-        youtube.get('/search', {
+    state = { videos: [], selectedVideo: null }
+
+    onTermSearch = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         })
+
+        // console.log(response.data.items)
+        this.setState({ videos: response.data.items });
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video })
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar anyNameOnFormSubmit={this.onTermSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
             </div>
         )
     }
