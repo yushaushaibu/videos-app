59517718c6c2e5d90d89cb70a0ce3c5583c1fe45
yushaushaibu <yushaushaibu1@gmail.com>
2022-08-11 16:55:15 -0700

    Video list and detail css grid layout using semantic ui

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,8 +4,13 @@ const VideoDetail = ({ video }) => {
     if (!video) {
         return <div>Loading..</div>
     }
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
     return (
         <div>
+            <div className="ui embed">
+                <iframe src={videoSrc} title="Video Player" />
+            </div>
             <div className="ui segment">
                 <h3 className="ui header">{video.snippet.title}</h3>
                 <p>{video.snippet.description}</p>
